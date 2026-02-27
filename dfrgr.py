6db1e08ad39e036f39c037ec03337264861d7271
ziyauddin01 <ziyauddinsoudagar832@gmail.com>
2026-02-27 12:55:28 +0530

    Add files via upload

---
+++ dfrgr.py
@@ -0,0 +1,8 @@
+colleges={"MLBP","BET","GPT","GIT"}
+print(colleges)
+colleges.append("BGS")
+print(colleges)
+colleges.insert(0,"BHARATESH")
+print(colleges)
+colleges.remove("GPT")
+print(colleges)
